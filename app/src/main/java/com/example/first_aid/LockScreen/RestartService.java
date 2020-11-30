312dc16d614f6145de8a4a972e4c6a1f44675063
SeoPPak <andrew201239@gmail.com>
2020-11-30 18:58:17 +0900

    Foreground 수정중

--- app/src/main/java/com/example/first_aid/LockScreen/RestartService.java
+++ app/src/main/java/com/example/first_aid/LockScreen/RestartService.java
@@ -5,23 +5,27 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-public class RestartService extends BroadcastReceiver {
 
+
+public class RestartService extends BroadcastReceiver {
     public static final String ACTION_RESTART_PERSISTENTSERVICE
             = "ACTION.RESTART.PersistentService";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        //TODO Auto-generated method stub
-        Log.d("ImmortalService", "RestartService called!@!@@@@@#$@$@#$@#$@#");
+        Log.d("RestartService", "RestartService called~");
 
-
+        /* 서비스 죽일때 다시 서비스 등록 */
         if(intent.getAction().equals(ACTION_RESTART_PERSISTENTSERVICE)) {
             Intent i = new Intent(context, PersistentService.class);
-            //Intent i = new Intent(this, PersistentService.class);
             context.startService(i);
-
         }
 
+        /* 폰 재부팅할때 서비스 등록 */
+        if(intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)){
+            Log.d("RestartService", "ACTION_BOOT_COMPLETED");
+            Intent i = new Intent(context, PersistentService.class);
+            context.startService(i);
+        }
     }
 }
