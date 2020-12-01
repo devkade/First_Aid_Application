5e08b5290f02416d0c5ed4ce63fb2ac2e3d04e33
SeoPPak <andrew201239@gmail.com>
2020-12-02 00:32:11 +0900

    Add Get display inform in background

---
+++ app/src/main/java/com/example/first_aid/LockScreen/RealService.java
@@ -0,0 +1,157 @@
+package com.example.first_aid.LockScreen;
+
+import android.app.AlarmManager;
+import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
+import android.os.IBinder;
+import androidx.core.app.NotificationCompat;
+
+import android.os.PowerManager;
+import android.widget.Toast;
+
+import com.example.first_aid.MainActivity;
+import com.example.first_aid.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class RealService extends Service {
+    private Thread mainThread;
+    public static Intent serviceIntent = null;
+
+    public RealService() {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        serviceIntent = intent;
+        showToast(getApplication(), "Start Service");
+
+        mainThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                SimpleDateFormat sdf = new SimpleDateFormat("aa hh:mm");
+                boolean run = true;
+                String on;
+                PowerManager pm = (PowerManager) getApplicationContext().getSystemService(POWER_SERVICE);
+                boolean screen;
+
+                Intent intent1 = new Intent(getApplicationContext(), LockScreenActivity.class);
+
+                while (run) {
+                    try {
+                        screen = pm.isScreenOn();
+
+                        if(screen)
+                            on = "True";
+                        else
+                            on = "False";
+
+                        Thread.sleep(1000 * 5); // 1 minute
+                        Date date = new Date();
+                        //showToast(getApplication(), sdf.format(date));
+                        sendNotification(on);
+                        if(!screen)
+                            startActivity(intent1);
+                    } catch (InterruptedException e) {
+                        run = false;
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        mainThread.start();
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        serviceIntent = null;
+        setAlarmTimer();
+        Thread.currentThread().interrupt();
+
+        if (mainThread != null) {
+            mainThread.interrupt();
+            mainThread = null;
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+
+    public void showToast(final Application application, final String msg) {
+        Handler h = new Handler(application.getMainLooper());
+        h.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(application, msg, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    protected void setAlarmTimer() {
+        final Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(System.currentTimeMillis());
+        c.add(Calendar.SECOND, 1);
+        Intent intent = new Intent(this, AlarmReceiver.class);
+        PendingIntent sender = PendingIntent.getBroadcast(this, 0,intent,0);
+
+        AlarmManager mAlarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        mAlarmManager.set(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), sender);
+    }
+
+    private void sendNotification(String messageBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        String channelId = "fcm_default_channel";//getString(R.string.default_notification_channel_id);
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(R.mipmap.ic_launcher)//drawable.splash)
+                        .setContentTitle("Service test")
+                        .setContentText(messageBody)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setPriority(Notification.PRIORITY_HIGH)
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,"Channel human readable title", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+}
--- app/src/main/java/com/example/first_aid/MainActivity.java
+++ app/src/main/java/com/example/first_aid/MainActivity.java
@@ -3,16 +3,29 @@ package com.example.first_aid;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.first_aid.BottomNavigationBarFragment.FragmentContents;
 import com.example.first_aid.BottomNavigationBarFragment.FragmentMain;
 import com.example.first_aid.BottomNavigationBarFragment.FragmentQuiz;
 import com.example.first_aid.BottomNavigationBarFragment.FragmentSetting;
+import com.example.first_aid.LockScreen.AlarmReceiver;
+import com.example.first_aid.LockScreen.RealService;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Intent serviceIntent;
 
     BottomNavigationView bottomNavigationView;
     FragmentMain fragmentMain;
@@ -24,6 +37,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+        PowerManager pm = (PowerManager) getApplicationContext().getSystemService(POWER_SERVICE);
+        boolean isWhiteListing = false;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            isWhiteListing = pm.isIgnoringBatteryOptimizations(getApplicationContext().getPackageName());
+        }
+        if (!isWhiteListing) {
+            Intent intent = new Intent();
+            intent.setAction(android.provider.Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+            intent.setData(Uri.parse("package:" + getApplicationContext().getPackageName()));
+            startActivity(intent);
+        }
+
+        if (RealService.serviceIntent==null) {
+            serviceIntent = new Intent(this, RealService.class);
+            startService(serviceIntent);
+        } else {
+            serviceIntent = RealService.serviceIntent;//getInstance().getApplication();
+            Toast.makeText(getApplicationContext(), "already", Toast.LENGTH_LONG).show();
+        }
+
+
+
+
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
         // 프래그먼트 생성
@@ -66,5 +106,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (serviceIntent!=null) {
+            stopService(serviceIntent);
+            serviceIntent = null;
+        }
     }
 }
