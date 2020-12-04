258d7e1dc037353a9a82689fade2809a6120924f
DESKTOP-HVLNUJ5\Mario76 <2001cupid@naver.com>
2020-12-05 00:46:20 +0900

    quiz_layout_4

--- app/src/main/java/com/example/first_aid/BottomNavigationBarFragment/FragmentQuiz.java
+++ app/src/main/java/com/example/first_aid/BottomNavigationBarFragment/FragmentQuiz.java
@@ -1,27 +1,42 @@
 package com.example.first_aid.BottomNavigationBarFragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-import com.example.first_aid.Quiz.ListViewBtnItem;
+import com.example.first_aid.Quiz.QuizActivity;
 import com.example.first_aid.R;
-
-import java.util.ArrayList;
 
 
 public class FragmentQuiz extends Fragment {
 
     @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quiz, container, false);
+        View view = inflater.inflate(R.layout.fragment_quiz, container, false);
+        Button O1 = (Button) view.findViewById(R.id.O1);
+        O1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent=new Intent(getActivity(),QuizActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 
 }
