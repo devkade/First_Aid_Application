cb37bbf67ffa079278c0171e5974c34289ef397c
jypaulsung <jypaulsung@gmail.com>
2020-12-05 01:37:35 +0900

    First Aid Test 1

--- app/src/main/java/com/example/first_aid/BottomNavigationBarFragment/FragmentContents.java
+++ app/src/main/java/com/example/first_aid/BottomNavigationBarFragment/FragmentContents.java
@@ -1,5 +1,6 @@
 package com.example.first_aid.BottomNavigationBarFragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,7 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.first_aid.FirstAid.FirstAidMainPage;
+import com.example.first_aid.FirstAid.FirstAidPage1;
 import com.example.first_aid.R;
 
 
@@ -17,6 +21,18 @@ public class FragmentContents extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contents, container, false);
+        View view = inflater.inflate(R.layout.first_aid_main_layout, container, false);
+
+        Button button = (Button) view.findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getActivity(), FirstAidPage1.class);
+                startActivity(in);
+            }
+        });
+
+        return view;
+
     }
 }
---
+++ app/src/main/java/com/example/first_aid/FirstAid/FirstAidPage1_1.java
@@ -0,0 +1,16 @@
+package com.example.first_aid.FirstAid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.first_aid.R;
+
+public class FirstAidPage1_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_aid_page1_1);
+    }
+}
