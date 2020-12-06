2eab919177495f6d4391909988671b547aad8bc5
jypaulsung <jypaulsung@gmail.com>
2020-12-06 18:02:52 +0900

    First Aid Page with Thumbnails

--- app/src/main/java/com/example/first_aid/BottomNavigationBarFragment/FragmentContents.java
+++ app/src/main/java/com/example/first_aid/BottomNavigationBarFragment/FragmentContents.java
@@ -9,8 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
-import com.example.first_aid.FirstAid.FirstAidMainPage;
 import com.example.first_aid.FirstAid.FirstAidPage1;
 import com.example.first_aid.R;
 
@@ -23,12 +23,24 @@ public class FragmentContents extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.first_aid_main_layout, container, false);
 
-        Button button = (Button) view.findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener(){
+        ImageButton imageButton1 = (ImageButton) view.findViewById(R.id.cprImageButton);
+
+        imageButton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent in = new Intent(getActivity(), FirstAidPage1.class);
-                startActivity(in);
+                Intent intentLoadPage1 = new Intent(getActivity(), FirstAidPage1.class);
+                startActivity(intentLoadPage1);
+            }
+
+        });
+
+        Button button1 = (Button) view.findViewById(R.id.cprButton);
+
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intentLoadPage1 = new Intent(getActivity(), FirstAidPage1.class);
+                startActivity(intentLoadPage1);
             }
         });
 
--- app/src/main/java/com/example/first_aid/FirstAid/FirstAidPage1.java
+++ app/src/main/java/com/example/first_aid/FirstAid/FirstAidPage1.java
@@ -6,28 +6,65 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.first_aid.R;
 
 public class FirstAidPage1 extends AppCompatActivity {
-    private Button button;
+    Button button1;
+    Button button2;
+    ImageButton imagebutton1;
+    ImageButton imagebutton2;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_aid_page1);
 
-        button = (Button) findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openFirstAidPage1_1();
             }
         });
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFirstAidPage1_2();
+            }
+        });
+
+        ImageButton imagebutton1 = (ImageButton) findViewById(R.id.adultcprImageButton);
+        imagebutton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLoad1_1 = new Intent(FirstAidPage1.this, FirstAidPage1_1.class);
+                startActivity(intentLoad1_1);
+            }
+
+        });
+
+        ImageButton imagebutton2 = (ImageButton) findViewById(R.id.childcprImageButton);
+        imagebutton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLoad1_2 = new Intent(FirstAidPage1.this, FirstAidPage1_2.class);
+                startActivity(intentLoad1_2);
+            }
+
+        });
     }
 
     public void openFirstAidPage1_1(){
-        Intent intent = new Intent(this, FirstAidPage1_1.class);
-        startActivity(intent);
+        Intent intentLoad1_1 = new Intent(this, FirstAidPage1_1.class);
+        startActivity(intentLoad1_1);
+    }
+
+    public void openFirstAidPage1_2(){
+        Intent intentLoad1_2 = new Intent(this, FirstAidPage1_2.class);
+        startActivity(intentLoad1_2);
     }
 }
