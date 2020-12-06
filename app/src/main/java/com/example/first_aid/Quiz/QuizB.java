86fa2190493d3c916cfaeb9aced1532a216a1fa9
DESKTOP-HVLNUJ5\Mario76 <2001cupid@naver.com>
2020-12-06 14:00:02 +0900

    quiz_8

--- app/src/main/java/com/example/first_aid/Quiz/QuizB.java
+++ app/src/main/java/com/example/first_aid/Quiz/QuizB.java
@@ -1,4 +1,89 @@
 package com.example.first_aid.Quiz;
 
-public class QuizB {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View.OnClickListener;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.first_aid.R;
+import com.example.first_aid.database.odapNote;
+import com.example.first_aid.database.oxquiz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuizB extends AppCompatActivity {
+    private final oxquiz oxquiz1 = new oxquiz();
+    private List<List<String>> setB = oxquiz1.getoxB();
+    public odapNote odapB=new odapNote();
+    private static int count;
+    Button O, X;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quiz_ox_layout);
+            TextView textView1 = (TextView) findViewById(R.id.quiz_category);
+            textView1.setText(setB.get(count).get(0));
+            TextView textView2 = (TextView) findViewById(R.id.quiz_num);
+            textView2.setText(setB.get(count).get(1));
+            TextView textView3 = (TextView) findViewById(R.id.quiz_question);
+            textView3.setText(setB.get(count).get(2));
+
+            O = (Button) findViewById(R.id.O);
+            O.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (setB.get(count).get(3) == "O") {
+                        Toast.makeText(QuizB.this, "정답입니다!", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        Toast.makeText(QuizB.this, "틀렸습니다. 답 : X", Toast.LENGTH_SHORT).show();
+                        odapB.setOdapB(setB.get(count));
+                    }
+                    count++;
+                    if(count<setB.size()) {
+                        Intent intent = new Intent(getApplicationContext(), QuizB.class);
+                        startActivity(intent);
+                    }
+                    else{
+                        Intent intent = new Intent(getApplicationContext(), ComB.class);
+                        startActivity(intent);
+                    }
+                }
+            });
+
+            X = (Button) findViewById(R.id.X);
+            X.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (setB.get(count).get(3) == "X") {
+                        Toast.makeText(QuizB.this, "정답입니다!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(QuizB.this, "틀렸습니다. 답 : O", Toast.LENGTH_SHORT).show();
+                        odapB.setOdapB(setB.get(count));
+                    }
+                    count++;
+                    if(count<setB.size()) {
+                        Intent intent = new Intent(getApplicationContext(), QuizB.class);
+                        startActivity(intent);
+                    }
+                    else{
+                        Intent intent = new Intent(getApplicationContext(), ComB.class);
+                        startActivity(intent);
+                    }
+                }
+            });
+
+    }
 }
+
+
+
+
+
+
--- app/src/main/java/com/example/first_aid/database/oxquiz.java
+++ app/src/main/java/com/example/first_aid/database/oxquiz.java
@@ -24,12 +24,35 @@ public class oxquiz {
 
     public List<List<String>> getoxB(){
         List<List<String>> B = new ArrayList<List<String>>();
-        for(int i=0; i<oxq.size(); i++) {
-            List<String> line = oxq.get(i);
-            if(line.get(0)=="B"){
-                B.add(line);
-            }
-        }
+        List<String> C = new ArrayList<String>();
+        C.add("B");
+        C.add("1");
+        C.add("영아(아기)에게 심폐소생술을 할 때 깍지를 끼고 흉부압박을 해야 한다. O/X");
+        C.add("X");
+        C.add("상당히 큰 부상이 발생할 수도 있다. 중약지를 가지고 압박해주어야 한다.");
+        B.add(C);
+        List<String> D = new ArrayList<String>();
+        D.add("B");
+        D.add("2");
+        D.add("영아(아기)가 목이 막힌 상황일 때(기도폐쇄) 등을 두드려야 한다. O/X");
+        D.add("O");
+        D.add("영아 기도폐쇄 상황에서 적합한 방법 중 하나이다.");
+        B.add(D);
+        List<String> E = new ArrayList<String>();
+        E.add("B");
+        E.add("3");
+        E.add("임산부나 복부비만 기도폐쇄시의 응급처치 방법은 다른 사람들과는 달리 가슴을 밀어내는(혹은 가슴압박) 방법을 써야 한다. O/X\n");
+        E.add("O");
+        E.add("단 일반인의 경우 복부 밀어내기법(일반적으로 아는 하임리히법)을 사용한다.");
+        B.add(E);
+        List<String> F = new ArrayList<String>();
+        F.add("B");
+        F.add("4");
+        F.add("기도 폐쇄로 의식을 잃은 경우 인공호흡을 해야 한다. O/X");
+        F.add("O");
+        F.add("의식을 잃은 경우 인공호흡과 하임리히법을 번갈아 가면서 해야 한다.");
+        B.add(F);
+
         return B;
     }
 
