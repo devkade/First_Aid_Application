e1cadcaacff360430fee520db711f7a1ab6c8ef7
YeongWonKim <wsx1386@khu.ac.kr>
2020-12-06 17:42:17 +0900

    이미지 uri 받아오기(이미지 띄우는 건 아직)

--- app/src/main/java/com/example/first_aid/BottomNavigationBarFragment/FragmentContents.java
+++ app/src/main/java/com/example/first_aid/BottomNavigationBarFragment/FragmentContents.java
@@ -1,21 +1,54 @@
 package com.example.first_aid.BottomNavigationBarFragment;
 
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.first_aid.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 
 public class FragmentContents extends Fragment {
 
+
+    //서버에서 이미지 연결
+    StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+    StorageReference photoReference= storageReference.child("1.jpg");
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        ImageView imgView = inflater.inflate(R.layout.fragment_contents, container, false).findViewById(R.id.imageView);
+
+        //서버에서 이미지 연결
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+        StorageReference photoReference= storageReference.child("1.jpg");
+        photoReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Log.d("성공",uri.toString());
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                Log.d("Test"," Failed!");
+            }
+        });
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_contents, container, false);
     }
--- app/src/main/java/com/example/first_aid/MainTabBar/MainNewsTab.java
+++ app/src/main/java/com/example/first_aid/MainTabBar/MainNewsTab.java
@@ -19,6 +19,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -63,6 +65,7 @@ String number = "1";
         });
 
         //끝
+
 
 
 
